3352dcaaab39bb62efec3b180a784f525ad76a93
sbin.jeon <sbin.jeon@samsung.com>
2024-07-03 14:53:12 +0900

    [feature] 27p

--- Project36/baseball.cpp
+++ Project36/baseball.cpp
@@ -1,3 +1,9 @@
-class Baseball {
+#include <stdexcept>
+using namespace std;
 
+class Baseball {
+public:
+	void guess(const string& str) {
+		throw length_error("Input must be three letters.");
+	}
 };
