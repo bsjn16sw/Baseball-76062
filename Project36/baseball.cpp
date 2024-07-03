6c1a1f4dead692170d10c549ea1bdee214e431e4
sbin.jeon <sbin.jeon@samsung.com>
2024-07-03 14:56:44 +0900

    [feature] 30p

--- Project36/baseball.cpp
+++ Project36/baseball.cpp
@@ -7,5 +7,10 @@ public:
 		if (str.length() != 3) {
 			throw length_error("Input must be three letters.");
 		}
+		for (char c : str) {
+			if (c < '0' || c > '9') {
+				throw invalid_argument("Input must be number.");
+			}
+		}
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,8 @@ TEST(BaseballGame, ThrowExcptInputhLenUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExcptInputhInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
