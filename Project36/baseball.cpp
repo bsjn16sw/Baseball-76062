e08f16937ae515f025c3a95e9af1cad2f5ae599e
sbin.jeon <sbin.jeon@samsung.com>
2024-07-03 15:06:37 +0900

    [refactor] 40-42p

--- Project36/baseball.cpp
+++ Project36/baseball.cpp
@@ -4,18 +4,25 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+	void assertIllegalArgument(const std::string& guessNumber)
+	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Input must be three letters.");
 		}
 		for (char c : guessNumber) {
-			if (c < '0' || c > '9') {
-				throw invalid_argument("Input must be number.");
-			}
+			if (c >= '0' && c <= '9') continue;
+			throw invalid_argument("Input must be number.");
 		}
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[1] == guessNumber[2]
-			|| guessNumber[2] == guessNumber[0]) {
+		if (hasSameNumber(guessNumber)) {
 			throw invalid_argument("Input must not contain same number.");
 		}
+	}
+	bool hasSameNumber(const std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[1] == guessNumber[2]
+			|| guessNumber[2] == guessNumber[0];
 	}
 };
