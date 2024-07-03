00abb7c1bdc7dd48d42682b4670a8ab5d73aee8b
sbin.jeon <sbin.jeon@samsung.com>
2024-07-03 15:54:24 +0900

    [test] 1S0B, 1S1B, 0S3B, 0S0B, 0S1B, 0S2B Test Success

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -61,3 +61,86 @@ TEST_F(BaseballFixture, Strikes1Balls2Answer) {
 	EXPECT_EQ(1, result.strikes);
 	EXPECT_EQ(2, result.balls);
 }
+
+TEST_F(BaseballFixture, Strikes1Balls0Answer) {
+	GuessResult result = game.guess("145");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(0, result.balls);
+
+	result = game.guess("425");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(0, result.balls);
+
+	result = game.guess("453");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, Strikes1Balls1Answer) {
+	GuessResult result = game.guess("142");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+
+	result = game.guess("134");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+
+	result = game.guess("324");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+
+	result = game.guess("421");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+
+	result = game.guess("243");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+
+	result = game.guess("413");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+
+}
+
+TEST_F(BaseballFixture, Strikes0Balls3Answer) {
+	GuessResult result = game.guess("231");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(3, result.balls);
+
+	result = game.guess("312");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(3, result.balls);
+}
+
+TEST_F(BaseballFixture, Strikes0Balls0Answer) {
+	GuessResult result = game.guess("456");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(0, result.balls);
+}
+
+TEST_F(BaseballFixture, Strikes0Balls1Answer) {
+	GuessResult result = game.guess("245");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(1, result.balls);
+}
+
+TEST_F(BaseballFixture, Strikes0Balls2Answer) {
+	GuessResult result = game.guess("234");
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(2, result.balls);
+}
